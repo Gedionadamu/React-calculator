d56db31e65cabd5b4e12b7c299045ddda549be83
Gedion Adamu <gedionadamu@gmail.com>
2022-10-31 09:16:58 -0400

    add javascript starter files.

---
+++ index.js
@@ -0,0 +1,9 @@
+const add = document.querySelector("#+");
+const sub = document.querySelector("#-");
+const div = document.querySelector("#/");
+const mlt = document.querySelector("#x")
+
+function oprations (){
+   
+     
+}
