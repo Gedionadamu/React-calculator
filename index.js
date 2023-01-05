ca785f073f5f9983ad8f6a7e3f7cb6f06c37eb55
Gedion Adamu <gedionadamu@gmail.com>
2023-01-05 17:52:56 -0500

    added calculation function

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const add = document.querySelector("#+");
-const sub = document.querySelector("#-");
-const div = document.querySelector("#/");
-const mlt = document.querySelector("#x")
+const buttons = document.querySelectorAll('button')
+const display = document.querySelector('.no_display')
 
-function oprations (){
+
+
+let calculation =[]
+let CollCal
+function calculate(button){
+    
+    const value = button.textContent
+    if(value == "Clear"){
+        console.log(value)
+        calculation=[]
+        display.value ="0"
+    } else if (value=="="){
+        let ans = eval(CollCal)
+        display.value = ans
+        calculation=[]
+        calculation.push(ans)
+        
+    } else{
+        calculation.push(value)
+        CollCal = calculation.join('')
+        display.value= CollCal
+    }
+    
    
-     
 }
+
+buttons.forEach(button=> button.addEventListener('click', ()=> calculate(button)))
